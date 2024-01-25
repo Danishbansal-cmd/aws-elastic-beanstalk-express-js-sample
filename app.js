371ade400163b2eff5e37e620c1ada97d7b144e4
Danish Bansal <danishbansal60@gmail.com>
2024-01-25 07:53:44 -0500

    commit first changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello this is the message used to display for the first time using aws codepipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
