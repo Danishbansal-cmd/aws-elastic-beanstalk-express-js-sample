d54d537933ae8bf364fbc1be7141319a484d61f1
Danish Bansal <danishbansal60@gmail.com>
2024-01-25 09:02:50 -0500

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello this is the message used to display for the first time using aws codepipeline'));
+app.get('/', (req, res) => res.send('Last change in the repo for the full pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
